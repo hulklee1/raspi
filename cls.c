2b54d2b08b85afce9d2d17828efa4e4b1b3061d0
hulklee1 <hulklee@gmail.com>
2024-07-02 14:10:43 +0900

    1

---
+++ cls.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int x = 10, y = 20;
+	printf("\033[2J");
+	while(1)
+	{
+		printf("\033[%d;%dH ", y, x); // [x;yH
+	}
+	return 0;
+}
--- i2c.c
+++ i2c.c
@@ -3,26 +3,26 @@
 #include <wiringPiI2C.h>
 int main()
 {
+	printf("\033[2J");  // clear screen
 	int fd = wiringPiI2CSetup(0x48);
 	
 	while(1)
 	{
-		wiringPiI2CWrite(fd, 00);
-		wiringPiI2CRead(fd);
-		int v1 = wiringPiI2CRead(fd);
+		wiringPiI2CWrite(fd, 00);	wiringPiI2CRead(fd); // set & clear
+		int x = (wiringPiI2CRead(fd) * 80) / 255;  // 0~255
 		delay(100);
 
 		wiringPiI2CWrite(fd, 01);
 		wiringPiI2CRead(fd);
-		int v2 = wiringPiI2CRead(fd);
+		int y = (wiringPiI2CRead(fd) * 24) / 255;
 		delay(100);
 
-		wiringPiI2CWrite(fd, 03);
-		wiringPiI2CRead(fd);
-		int v3 = wiringPiI2CRead(fd);
-		delay(100);
-		
-		printf("ADC values : %d   %d   %d\n", v1, v2, v3);
+		//wiringPiI2CWrite(fd, 03);
+		//wiringPiI2CRead(fd);
+		//int v3 = wiringPiI2CRead(fd);
+		//delay(100);
+		printf("\033[%d;%dH(%d,%d)", y, x, x, y);
+		delay(200);
 	}
 	
 	return 0;
